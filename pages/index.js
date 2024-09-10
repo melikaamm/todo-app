d76790161841cc33c2adbf61b117082c70d53f66
melikaamm <113208136+melikaamm@users.noreply.github.com>
2024-09-10 22:42:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ function Home({ todos }) {
     return (
         <>
             <Head>
-                <title>Todos || Melika</title>
+                <title>Todos || Home</title>
                 <meta name='description' content='Todos || Manage your tasks easily' />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
             </Head>
